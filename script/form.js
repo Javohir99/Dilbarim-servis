f77c76e2966db2ce6755f44e91feaa9d69fab05b
Javohir99 <shavkatov.j.x@gmail.com>
2023-12-08 16:49:44 +0500

    input script

---
+++ script/form.js
@@ -0,0 +1,47 @@
+const telephone = document.querySelector('#phone');
+const adv = document.querySelector('#adv');
+const course = document.querySelector('#course');
+function etc(event){
+    const area = event.target;
+    if(area.value == 'etc'){
+            const row = document.createElement('div');
+            const label = document.createElement('label');
+            const input = document.createElement('input');
+
+            row.classList.add('row','py-3');
+            label.classList.add('col-3','text-center','offset-1');
+            input.classList.add('col-3','offset-4');
+
+            label.innerHTML = '<i>'+area.previousElementSibling.innerHTML+'</i>';
+
+            input.setAttribute('type','text');
+            row.append(label);
+            row.append(input);
+            area.parentElement.after(row);
+    }
+    else{
+        const nextElement = area.parentElement.nextElementSibling;
+        if(nextElement.firstElementChild.innerHTML == '<i>'+area.previousElementSibling.innerHTML+'</i>'){
+            nextElement.remove();
+        }
+    }
+}
+adv.addEventListener('change',etc);
+course.addEventListener('change',etc);
+
+telephone.addEventListener('focus',()=>{
+    telephone.value = '+998 (';
+});
+telephone.addEventListener('input',()=>{
+    switch(telephone.value.length){
+        case 8:
+            telephone.value = value = telephone.value + ') ';
+        break;
+        case 13:
+            telephone.value = telephone.value + '-';
+            break;
+        case 16:
+            telephone.value = telephone.value + '-';
+            break;
+    }
+})
