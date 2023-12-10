5a6a1c084d2f3c3ad84fc636115f0148d8e312f5
Javohir99 <shavkatov.j.x@gmail.com>
2023-12-11 00:33:58 +0500

    DB module and registration.ejs

---
+++ DB.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+/*JSON format for ragistration.json {"students":[]}*/
+const DB = {
+  DBpath: './date/registration.json',
+  WriteDate: (date) => {
+    fs.open(DB.DBpath, 'r+', (err, fd) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      fs.read(fd, (err, bytesRead, buffer) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        const content = buffer.slice(0, bytesRead).toString();
+        const jsonData = JSON.parse(content);
+        date.data = new Date().toLocaleString();
+        jsonData.students.push(date);
+        const updatedContent = JSON.stringify(jsonData);
+
+        fs.write(fd, updatedContent, 0, (err, bytesWritten, buffer) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+
+          fs.close(fd, (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+          });
+        });
+      });
+    });
+  },
+  ShowAll:()=>{
+    return JSON.parse(fs.readFileSync(DB.DBpath,'utf-8'));    
+  }
+}
+module.exports = DB;
--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const fs = require('fs');
-const JSONKA = `[{"secondname":"Shavkatov","firstname":"Javoxir","phone":"+998 (94) 608-24-42","course":"compyuter","adv":"familiar","adress":"Farobiy"}]`;
-const obj = JSON.parse(JSONKA);
-const PORT = 3000;
+const DB = require('./DB.js');
+const PORT = 2222;
 
 app.set('veiw engine','ejs');
 
@@ -12,53 +10,11 @@ app.set('veiw engine','ejs');
 
 const CreatePath = (file) => path.resolve(__dirname,'views',file + '.ejs');
 
-const WriteDate = (date)=>{
-    fs.open('./date/student.json', 'r+', (err, fd) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-      
-        fs.read(fd, (err, bytesRead, buffer) => {
-        console.log(buffer.toString());
-          if (err) {
-            console.error(err);
-            return;
-          }
-      
-          const content = buffer.slice(0, bytesRead).toString();
-          const jsonData = JSON.parse(content);
-          jsonData.student.push(date);
-          const updatedContent = JSON.stringify(jsonData);
-      
-          fs.write(fd, updatedContent,0,(err, bytesWritten, buffer) => {
-            if (err) {
-              console.error(err);
-              return;
-            }
-      
-            fs.close(fd, (err) => {
-              if (err) {
-                console.error(err);
-                return;
-              }
-              console.log('Файл успешно закрыт.');
-            });
-          });
-        });
-      });
-}
-
 app.use(express.static('style'));
 app.use(express.static('script'));
 app.use(express.static('img'));
 
 app.use(express.urlencoded({extended:false}));
-
-app.post('/addstudent',(req,res)=>{
-    WriteDate(req.body)
-    res.send(req.body);
-});
 
 app.get('/', (req, res) => {
     res.render(CreatePath('index'));
@@ -73,11 +29,17 @@ app.get('/students', (req, res) => {
 });
 
 app.get('/registration', (req, res) => {
-    res.render(CreatePath('registration'));
+    const date = DB.ShowAll(); 
+    res.render(CreatePath('registration'),{date});
 });
 
 app.get('/addstudent', (req, res) => {
     res.render(CreatePath('addstudent'));
+});
+
+app.post('/addstudent',(req,res)=>{
+    DB.WriteDate(req.body);
+    res.render(CreatePath('index'));
 });
 
 app.listen(PORT,'localhost',()=>{
